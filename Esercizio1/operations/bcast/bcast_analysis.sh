fa8caf9b8299faa61a5c64b4170e8830d1bcff0d
YasminBougammoura <yasmin.bougammoura@gmail.com>
2024-11-04 14:13:47 +0100

    Updating folder 0

---
+++ Esercizio1/operations/bcast/bcast_analysis.sh
@@ -0,0 +1,6 @@
+for warmup in 100 200 300; do
+	for iter in 500 1000 1500 2000; do
+		analysis_results="benchmark_${warmup}w_${iter}i.txt"
+		mpirun osu_bcast -x $warmup -i $iter -f > $analysis_results
+	done
+done
